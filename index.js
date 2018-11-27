31f63afe3197f40fdbaecf4dad8e58ad64092e53
hassan aboueldahab <hassanaboueldahab@gmail.com>
2018-11-27 16:31:14 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
+
+
+
+
+class App extends React.Component{
+  render() {
+    return (
+      <View>
+        <StatusBar hidden={true} />
+
+        <Text style={styles.defaultText}>
+        hel!!!
+        </Text>
+        <Text style={[styles.defaultText,styles.selectedText]}>
+        Bonjour le mounde!!!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles= StyleSheet.create({
+  
+    defaultText:{
+        fontSize:20,
+        padding:100,
+        margin:20,
+        borderWidth:StyleSheet.hairlineWidth,
+        color:'red'
+    },
+    
+    selectedText:{
+        backgroundColor:'yellow',
+        color:'blue',
+        fontWeight:'bold'
+    }
+    })
+AppRegistry.registerComponent('albums', () => App);
